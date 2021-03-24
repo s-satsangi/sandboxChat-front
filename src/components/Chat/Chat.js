d2ce111ded19aa3eabb9aeaf335fae8919677d33
s-satsangi <s.satsangi@gmail.com>
2021-03-23 23:29:01 -0400

    got sockets set up with backend

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
 import "./Chat.css";
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000";
+
+  var connectionOptions = {
+    "force new connection": true,
+    reconnectionAttempts: "Infinity",
+    timeout: 10000,
+    transports: ["websocket"],
+  };
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT, connectionOptions);
+
+    setName(name);
+    setRoom(room);
+
+    console.log(socket);
+  }, [ENDPOINT, location.search]);
+
   return <h1>Chat</h1>;
 };
 
